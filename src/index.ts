09b0928ac840b6229204ff5a32063f6779bd004e
Ben <Koppe.Development@gmail.com>
2025-04-12 19:23:49 -0400

    Add index.ts entrypoint

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { FirebirdAdapter } from "./dialect/adapter.ts";
+export { FirebirdConnection } from "./dialect/connection.ts";
+export { FirebirdDialect } from "./dialect/dialect.ts";
+export type { FirebirdDialectConfig } from "./dialect/dialect.ts";
+export { FirebirdDriver } from "./dialect/driver.ts";
+export { FirebirdIntrospector } from "./dialect/introspector.ts";
+export { FirebirdQueryCompiler } from "./dialect/query-compiler.ts";
