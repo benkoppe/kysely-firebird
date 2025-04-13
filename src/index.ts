ff3b9708934c0b878ffdf300968f671fbc590d1d
Ben <Koppe.Development@gmail.com>
2025-04-13 02:58:40 -0400

    Add Firebird types to export

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,8 @@ export type { FirebirdDialectConfig } from "./dialect/dialect.js";
 export { FirebirdDriver } from "./dialect/driver.js";
 export { FirebirdIntrospector } from "./dialect/introspector.js";
 export { FirebirdQueryCompiler } from "./dialect/query-compiler.js";
+export type {
+  FirebirdPool,
+  FirebirdDb,
+  FirebirdTransaction,
+} from "./dialect/firebird.js";
